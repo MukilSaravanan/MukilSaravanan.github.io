d8c2786fd534d2de22b2e3c8e96970e3ed0b70eb
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2023-01-08 16:31:50 +0000

    Added Transition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log(123)
+let imageIndex = 2
+let numberOfBackgroundImage = 4;
+function backgroundChange() {
+    let backgroundImage = document.querySelector(".home-page-background")
+    console.log(backgroundImage)
+    if (backgroundImage) {
+        backgroundImage.setAttribute("src", `./assets/background-image-${imageIndex}.jpg`)
+    }
+    imageIndex = imageIndex >= numberOfBackgroundImage ? 1 : imageIndex + 1;
+    console.log('1234')
+}
+
+window.onload = function () {
+    setInterval(backgroundChange, 4000);
+};

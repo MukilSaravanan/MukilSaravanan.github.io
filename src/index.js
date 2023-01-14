5d1d1374cddff92ddc0dc51e80b732734a5fd4a7
Mukil Saravanan <senjerimalaimukil@gmail.com>
2023-01-14 15:23:51 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let imageIndex = 0
+
+// console.log(12)
+function backgroundChange(){
+    let backgroundImage = document.querySelectorAll(".home-page-background")
+    backgroundImage[imageIndex].classList.remove("visible-image")
+    backgroundImage[imageIndex].classList.add("hide-image")
+    imageIndex = imageIndex >= backgroundImage.length - 1? 0 : imageIndex + 1;
+    console.log(imageIndex)
+    backgroundImage[imageIndex].classList.remove("hide-image")
+    backgroundImage[imageIndex].classList.add("visible-image")
+}
+
+window.onload = function () {
+    setInterval(()=>{
+        backgroundChange()
+    }, 5000);
+};

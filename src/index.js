e9eb525744a18aedfcc4ad84012c4cb0665e41b2
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2023-01-27 17:02:19 +0000

    added timeline

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 let imageIndex = 0
 
 // console.log(12)
-function backgroundChange(){
+function backgroundChange() {
     let backgroundImage = document.querySelectorAll(".home-page-background")
     backgroundImage[imageIndex].classList.remove("visible-image")
     backgroundImage[imageIndex].classList.add("hide-image")
-    imageIndex = imageIndex >= backgroundImage.length - 1? 0 : imageIndex + 1;
+    imageIndex = imageIndex >= backgroundImage.length - 1 ? 0 : imageIndex + 1;
     console.log(imageIndex)
     backgroundImage[imageIndex].classList.remove("hide-image")
     backgroundImage[imageIndex].classList.add("visible-image")
 }
 
 window.onload = function () {
-    setInterval(()=>{
+    setInterval(() => {
         backgroundChange()
     }, 5000);
 };
+
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+    if (navWrapper.classList.contains("active")) {
+        this.setAttribute("aria-expanded", "false");
+        this.setAttribute("aria-label", "menu");
+        navWrapper.classList.remove("active");
+    } else {
+        navWrapper.classList.add("active");
+        this.setAttribute("aria-label", "close menu");
+        this.setAttribute("aria-expanded", "true");
+    }
+});

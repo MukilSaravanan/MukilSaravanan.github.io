8434ec53845d242336d48550f32a7d003851fa58
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2023-02-17 18:41:21 +0000

    added honours image popup

---
+++ js/honours.js
@@ -0,0 +1,36 @@
+// function NavMenuClick(){
+//     let mainNav = document.getElementById("mainNav")
+//     let navMenuButton = document.querySelector(".navbar-toggler")
+//     navMenuButton.addEventListener("click",()=>{
+//         if (mainNav.classList.contains("visible")) {
+//             mainNav.classList.remove("visible");
+//             mainNav.classList.add("hidden");
+//           } else {
+//             mainNav.classList.remove("hidden");
+//             mainNav.classList.add("visible");
+//           }
+//     })
+// }
+// NavMenuClick()
+
+window.onload = () => {
+    let thumpnailImage = document.querySelectorAll(".thumbnail>img")
+    thumpnailImage.forEach((image) => {
+        image.addEventListener("click",()=>{
+            let postDiv = image.parentElement.parentElement
+            let popUpDiv = postDiv.querySelector(".popup-thumbnail")
+            popUpDiv.classList.add("active")
+            let popUpClose = postDiv.querySelector(".popup-close")
+            popUpClose.addEventListener("click",()=>{
+                popUpDiv.classList.remove("active")
+            })
+        })
+    })
+
+    popUpClose.forEach((close)=>{
+        close.addEventListener("click",()=>{
+            
+        })
+    })
+
+}

d28d3f9a1198123041de55e7421a518c08608e58
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2023-03-19 15:34:32 +0000

    scroll to top added

--- js/index.js
+++ js/index.js
@@ -10,17 +10,17 @@ function backgroundChange() {
     backgroundImage[imageIndex].classList.add("visible-image")
 }
 
-function NavMenuClick(){
+function NavMenuClick() {
     let mainNav = document.getElementById("mainNav")
     let navMenuButton = document.querySelector(".navbar-toggler")
-    navMenuButton.addEventListener("click",()=>{
+    navMenuButton.addEventListener("click", () => {
         if (mainNav.classList.contains("visible")) {
             mainNav.classList.remove("visible");
             mainNav.classList.add("hidden");
-          } else {
+        } else {
             mainNav.classList.remove("hidden");
             mainNav.classList.add("visible");
-          }
+        }
     })
 }
 
@@ -28,10 +28,20 @@ window.onload = function () {
     NavMenuClick()
     const showButton = () => document.querySelector(".scroll-to-top").classList.add("visible");
     const hideButton = () => document.querySelector(".scroll-to-top").classList.remove("visible");
-    document.addEventListener("scroll", (e) => window.scrollY < 1000 ? hideButton() : showButton());
+    document.addEventListener("scroll", (e) =>{
+        console.log(123)
+        window.scrollY < 1000 ? hideButton() : showButton()
+    });
     setInterval(() => {
         backgroundChange()
     }, 5000);
+    scrollToTop()
 };
 
 
+function scrollToTop() {
+    const scrollToTopButton = document.querySelector(".scroll-to-top")
+    scrollToTopButton.addEventListener("click",()=>{
+        window.scrollTo(0, 0);
+    })
+}
--- js/project.js
+++ js/project.js
@@ -58,4 +58,17 @@ window.onload = function () {
             target: '.post'
         }
     });
+    scrollToTop();
 };
+
+function scrollToTop() {
+    const showButton = () => document.querySelector(".scroll-to-top").classList.add("visible");
+    const hideButton = () => document.querySelector(".scroll-to-top").classList.remove("visible");
+    document.addEventListener("scroll", (e) =>{
+        window.scrollY < 1000 ? hideButton() : showButton()
+    });
+    const scrollToTopButton = document.querySelector(".scroll-to-top")
+    scrollToTopButton.addEventListener("click",()=>{
+        window.scrollTo(0, 0);
+    })
+}

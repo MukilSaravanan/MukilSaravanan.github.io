e8e94acf6ee200f2f632b0a5f1e536a17543a691
Mohamed Asif S M <76587911+asifmd2001@users.noreply.github.com>
2023-03-26 18:38:36 +0000

    Fixed Minor issues

--- js/contact.js
+++ js/contact.js
@@ -4,4 +4,18 @@ window.addEventListener("load", () => {
         navigator.clipboard.writeText("mukil.saravanan.edu@gmail.com");
         window.alert("Email is copied to the clipboard")
     })
+    NavMenuClick();
+    function NavMenuClick(){
+        let mainNav = document.getElementById("mainNav")
+        let navMenuButton = document.querySelector(".navbar-toggler")
+        navMenuButton.addEventListener("click",()=>{
+            if (mainNav.classList.contains("visible")) {
+                mainNav.classList.remove("visible");
+                mainNav.classList.add("hidden");
+              } else {
+                mainNav.classList.remove("hidden");
+                mainNav.classList.add("visible");
+              }
+        })
+    }
 })
